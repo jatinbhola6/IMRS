257a14636671ae7bbb6c5f3fb5115855db2766ba
jatinbhola6 <jatinbhola6@gmail.com>
2017-03-30 21:36:47 +0530

    Final Classes to be called directly by API. Yet to be tested

---
+++ ALS.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+class ALS_algo():
+	def get_error(Q, X, Y, W):
+		#return mean error of predictions
+		return np.sum((W * (Q - np.dot(X, Y)))**2)
+	def get_weighted_factors(file_name = 'u1.base1',X_file='X.npy',Y_file='Y.npy',lambda_ = 0.1,n_factors = 100,n_iterations = 20,max_rating=5):
+		#Alternating Least Squares Algorithm
+		#for reference visit - https://bugra.github.io/work/notes/2014-04-19/alternating-least-squares-method-for-collaborative-filtering/ 
+		ratings = pd.read_csv('ml-100k/'+ file_name)
+		Q = ratings.loc[:,str(1):].values
+		W = Q>0.5	
+		W[W == True] = 1
+		W[W == False] = 0
+		W = W.astype(np.float64,copy = False)
+		self.W = W
+		m, n = Q.shape
+		X = max_rating * np.random.rand(m, n_factors) 
+		Y = max_rating * np.random.rand(n_factors, n)
+		weighted_errors = []
+		for ii in range(n_iterations):
+			for u, Wu in enumerate(W):
+				X[u] = np.linalg.solve(np.dot(Y, np.dot(np.diag(Wu), Y.T)) + lambda_ * np.eye(n_factors),np.dot(Y, np.dot(np.diag(Wu), Q[u].T))).T
+			for i, Wi in enumerate(W.T):
+				Y[:,i] = np.linalg.solve(np.dot(X.T, np.dot(np.diag(Wi), X)) + lambda_ * np.eye(n_factors),np.dot(X.T, np.dot(np.diag(Wi), Q[:, i])))
+			weighted_errors.append(get_error(Q, X, Y, W))
+			print('{}th iteration is completed'.format(ii))
+		weighted_Q_hat = np.dot(X,Y)
+		#np.save('q_hat',weighted_Q_hat)
+		np.save(X_file,X)
+		np.save(Y_file,Y)
+
+	def get_recommendations(user_id,no_recom=5,max_rating=5,X_file='X.npy',Y_file='Y.npy'):
+		#call this to get recommendations
+		#User ID is necessary argument.
+		X = np.load(X_file)
+		Y = np.load(Y_file)
+		Q_hat = np.dot(X,Y)[user_id-1]
+		Q_hat -= np.min(Q_hat)
+		Q_hat *= float(max_rating) / np.max(Q_hat)
+		top_recommendations = np.argsort(Q_hat-max_rating*self.W[user_id-1])[::-1][:no_recom]
+		movie_data = pd.read_csv('test.csv')
+		movie_ids = [movie_data.iloc[i]['_id'] for i in top_recommendations]
+		return movie_ids
+	def stream_ALS(user_id,movie_id,rating,file_name='u1.base1',X_file='X.npy',Y_file='Y.npy',lambda_=0.1):
+		#call this when new rating is recieved in the database.
+		#for reference visit - https://stanford.edu/~rezab/classes/cme323/S15/notes/lec14.pdf
+		X = np.load(X_file)
+		Y = np.load(Y_file)
+		ratings = pd.read_csv('ml-100k/'+ file_name)
+		movie_index = ratings.columns.get_loc(str(movie_id))
+		user_index = user_id-1
+		Xu = X[user_index]
+		Yi = Y[:,movie_index]
+		Xu_ = Xu - (rating - np.dot(Xu,Yi))*Yi.T*lambda_ + lambda_*Xu
+		Yi_ = Yi - (rating-np.dot(Xu,Yi))*Xu.T*lambda_ + lambda_*Yi
+		X[user_index] = Xu_
+		Y[:,movie_index] = Yi_
+		np.save(X_file,X)
+		np.save(Y_file,Y)
---
+++ content_based_filtering.py
@@ -0,0 +1,58 @@
+# coding=UTF-8 
+import pandas as pd
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+class CB_Filtering():
+	def converting_to_dummy_variable(file_name = 'test.csv',movie_features=['_id','director','creator','actor_list','genre'],out_file='dummy_data_for_clustering.csv'):
+		#this method converts data features to dummy features
+		"""
+		  _______________					 ___________
+	   	 |A_____|B_______|  is converted to |a|b|c|d|e|f| 
+	   	1|a,b___|d,f_____|				   1|1|1|0|1|0|1|
+		2|b,c___|d,e_____|				   2|0|1|1|1|1|0|
+		"""
+		movie_data = pd.read_csv(file_name,usecols = movie_features)
+		movie_data[movie_features] = movie_data[movie_features].apply(lambda x:x.str.replace('“','"')).apply(lambda x:x.str.replace('”','"'))
+		movie_data[movie_features] = movie_data[movie_features].applymap(lambda x:ast.literal_eval(x))
+		dummy_data = pd.DataFrame(movie_data[['_id']])
+		for feature in movie_features[1:]:
+			dummy_data = dummy_data.merge(movie_data[feature].str.join(sep='|').str.get_dummies(sep='|'),left_index=True,right_index=True)
+		dummy_data.to_csv(out_file)
+
+	def similarity_matrix(file_name='dummy_data_for_clustering.csv',def_index='_id',sim_file='similarity_matrix.npy'):
+		#this calculates similarty of each movie with each movie
+		#if m = number of movies and f = no. of features
+		#and our data is of form mxf then it gives us a 2d matrix of form mxm 
+		dummy_data = pd.read_csv(file_name)
+		dummy_data = dummy_data.iloc[:,1:].set_index(def_index)	
+		distances = cosine_similarity(dummy_data,dummy_data)
+		np.save(sim_file,distances)
+
+	def get_recommendations(movie_id,sim_file='similarity_matrix.npy',movie_file='test.csv',no_recom=5):
+		#it gives recommendations from movie id.
+		#Movie ID is necesssary argument for this recommendation
+		cos_sim = np.load(sim_file)
+		movie_list = pd.read_csv(movie_file)
+		movie_index = movie_list.index.get_loc(movie_id)
+		top_recommendations = np.argsort(cos_sim[movie_index])[::-1][1:6]
+		recom_ids = [movie_list.iloc[i]['_id'] for i in top_recommendations]
+		return recom_ids
+
+	def add_new_movie(movie_id,movie_file='test.csv',movie_features=['_id','director','creator','actor_list','genre'],dummy_file = 'dummy_data_for_clustering.csv'):
+		#this functions add new movie entries in our database to our dataframes. 
+		print _file
+		if not hasattr(movie_id,'__iter__') :
+			movie_id = [movie_id]
+		else:
+			movie_id = list(movie_id)
+		movie_data = pd.read_csv(movie_file)
+		new_df = movie_data.loc[movie_id]
+		new_df[movie_features] = new_df[movie_features].apply(lambda x:x.str.replace('“','"')).apply(lambda x:x.str.replace('”','"'))
+		new_df[movie_features] = new_df[movie_features].applymap(lambda x:ast.literal_eval(x))
+		dummy_data = pd.DataFrame(new_df[['_id']])
+		for feature in movie_features[1:]:
+			dummy_data = dummy_data.merge(new_df[feature].str.join(sep='|').str.get_dummies(sep='|'),left_index=True,right_index=True)
+		old_dummy = pd.DataFrame(_file)
+		old_dummy = old_dummy.append(dummy_data)
+		old_dummy.to_csv(dummy_file)
+    	
